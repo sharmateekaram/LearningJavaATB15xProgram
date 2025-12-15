2839614fe16d10e0b186e6047bdb993a091dde9c
MrAkashBhardwaj <mrbhardwaj.akash@gmail.com>
2025-12-15 08:57:35 +0530

    commit2

---
+++ src/ex_02_Java_Basics/Lab004_Comment.java
@@ -0,0 +1,7 @@
+package ex_02_Java_Basics;
+
+public class Lab004_Comment {
+    public static void main() {
+        //this is my main method
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab005_MultiComments.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab005_MultiComments {
+
+    public static void main() {
+       /* this is my main method
+        This is used to run further method*/
+    }
+
+}
