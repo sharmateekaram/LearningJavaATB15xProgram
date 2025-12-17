249944e0c0f969c22e4b3cd1292e207e022c326f
MrAkashBhardwaj <mrbhardwaj.akash@gmail.com>
2025-12-17 08:27:48 +0530

    printf

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -3,5 +3,11 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     public static void main(String[] args){
         System.out.println("Jai Shree Ram");
+
+        double a= 3.2222323f;
+        char c=1237;
+        int b= 9;
+        System.out.printf("%d*1=%d",b,b*1);
+
     }
 }
