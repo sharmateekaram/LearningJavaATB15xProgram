ff8f7e69b02609c85cc675a4d8998a8712083e68
MrAkashBhardwaj <mrbhardwaj.akash@gmail.com>
2026-01-09 08:17:48 +0530

    calculator with method input

--- src/ex_02_Java_Basics/Akki.java
+++ src/ex_02_Java_Basics/Akki.java
@@ -5,37 +5,6 @@ import java.util.Scanner;
 
 public class Akki {
 
-    static void main(String[] args) {
 
 
-        Random random = new Random();
-        int numToGuess = random.nextInt(100);
-        //System.out.println("Guess Num is : "+ guessNum);
-        System.out.println("Please guess a number");
-        Scanner scanner = new Scanner(System.in);
-        int guessedNum;
-        int attempts = 0;
-
-        while (true){
-            //to handle if user enter anything else apart from an integer
-
-            if(!scanner.hasNextInt()){
-                System.out.println("Invalid entry !!, Please enter an integer only.");
-                scanner.next(); //again asking for entry
-                continue;
-            }
-
-
-            guessedNum = scanner.nextInt();
-            attempts++;
-            if(guessedNum > numToGuess){
-                System.out.println("Entered number is too High");
-            } else if (guessedNum < numToGuess) {
-                System.out.println("Entered number is too Low");
-            }else{
-                System.out.println("Correct, you have taken :" + attempts);
-            }
-
-        }
-    }
 }
---
+++ src/ex_10_Methods/Lab00_Method_Calculator_UserEnterMethodName.java
@@ -0,0 +1,62 @@
+package ex_10_Methods;
+
+import java.util.Scanner;
+
+public class Lab00_Method_Calculator_UserEnterMethodName {
+
+    static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = readInt(scanner, "Enter The First Num");
+        int b = readInt(scanner, "Enter The Second Num");
+
+        System.out.println("Enter Calculation Method Name");
+        String methodName = scanner.next();
+        if(methodName.toLowerCase().matches("^[a-zA-Z]+$")){
+            int ans= allInOne(a,b,methodName);
+            System.out.println(methodName + " Of Two Num Is : "+ ans);
+        }else {
+            System.out.println("Bro Method name should be string!!!");
+        }
+        scanner.close();
+
+    }
+
+    private static int readInt(Scanner scanner, String prompt) {
+        System.out.println(prompt);
+        if (scanner.hasNextInt()){
+            return scanner.nextInt();
+        }else{
+            System.out.println("Please enter integer number only");
+            System.exit(0);
+            return  0;
+        }
+    }
+
+
+    static  int  allInOne(int a, int  b, String methodName){
+
+        int anstoReturn=0;
+        switch (methodName.toLowerCase()){
+            case "sum":
+                anstoReturn=  a+b;
+                break;
+            case "subtract":
+                anstoReturn= a-b;
+                break;
+            case "multiply":
+                anstoReturn= a*b;
+                break;
+            case "division":
+                anstoReturn= a/b;
+                break;
+            case "modulus":
+                anstoReturn= a%b;
+                break;
+            default:
+                System.out.println("Enter valid method name");
+                break;
+        }
+        return anstoReturn;
+    }
+}
